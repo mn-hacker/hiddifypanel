c7c71e1134794f5d817801cc01060f3f56174830
mn-hacker <mahdishokri228@outlook.com>
2026-01-01 03:05:39 +0330

    Fix: Translations and max_ips field improvements

--- hiddifypanel/panel/admin/UserAdmin.py
+++ hiddifypanel/panel/admin/UserAdmin.py
@@ -38,7 +38,7 @@ class UserAdmin(AdminLTEModelView):
     }
     list_template = 'model/user_list.html'
 # "max_ips",
-    form_columns = ["name","comment", "usage_limit", "reset_usage", "package_days", "reset_days", "mode", "max_ips", "uuid", "enable"]
+    form_columns = ["name","comment", "usage_limit", "max_ips", "reset_usage", "package_days", "reset_days", "mode", "uuid", "enable"]
     # form_excluded_columns = ['current_usage', 'monthly', 'telegram_id', 'last_online', 'expiry_time', 'last_reset_time', 'current_usage_GB',
     #  'start_date', 'added_by', 'admin', 'details', 'max_ips', 'ed25519_private_key', 'ed25519_public_key', 'username', 'password']
     page_size = 50
@@ -63,7 +63,8 @@ class UserAdmin(AdminLTEModelView):
     }
     form_args = {
         'max_ips': {
-            'validators': [NumberRange(min=3, max=10000)]
+            'validators': [NumberRange(min=0, max=10000)],
+            'description': _("حداکثر تعداد اتصالات همزمان کاربر. 0 یعنی بدون محدودیت. فقط در هسته Xray پشتیبانی می‌شود.")
         },
         'mode': {'enum': UserMode},
         'uuid': {
