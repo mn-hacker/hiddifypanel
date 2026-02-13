79c6cd94b7f49bffaf5dc53d0b765937203844c3
mn-hacker <mahdishokri228@outlook.com>
2026-02-13 17:49:45 +0330

    v12.0.24: Fix Truncate Command Error (500)

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # import importlib.metadata
 from datetime import datetime
 
-__version__ = '12.0.23'
-__release_time__= datetime.strptime('2026-02-13T17:20:00','%Y-%m-%dT%H:%M:%S')
+__version__ = '12.0.24'
+__release_time__= datetime.strptime('2026-02-13T17:45:00','%Y-%m-%dT%H:%M:%S')
 is_released_version = True
