fe1010f51de0189ada88951a7e5ee43b2aa9b706
mn-hacker <mahdishokri228@outlook.com>
2026-02-13 23:52:12 +0330

    Release v12.1.0

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # import importlib.metadata
 from datetime import datetime
 
-__version__ = '12.0.40b'
-__release_time__= datetime.strptime('2026-02-13T23:45:00','%Y-%m-%dT%H:%M:%S')
+__version__ = '12.1.0'
+__release_time__= datetime.strptime('2026-02-14T00:00:00','%Y-%m-%dT%H:%M:%S')
 is_released_version = True
