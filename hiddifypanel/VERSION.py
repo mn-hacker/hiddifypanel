dabd2f7fba6ab19170995ce205a2b42011bec052
mn-hacker <mahdishokri228@outlook.com>
2026-02-15 23:32:11 +0330

    v12.1.13: Final release

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.1.12b'
-__release_date__ = '2026-02-15 21:00'
+__version__ = '12.1.13'
+__release_date__ = '2026-02-16 23:30'
 __release_time__ = __release_date__
-is_released_version = False
+is_released_version = True
