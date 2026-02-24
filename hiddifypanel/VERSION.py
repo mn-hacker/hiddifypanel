bbc2c54cfb8ab60b78987d8f66e57c8bb333dac1
mn-hacker <mahdishokri228@outlook.com>
2026-02-24 18:13:42 +0330

    v12.1.28b: Remove amneziawg filter + CLI v4.0.4 native support

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.1.27b'
-__release_date__ = '2026-02-21 00:00'
+__version__ = '12.1.28b'
+__release_date__ = '2026-02-24 18:10'
 __release_time__ = __release_date__
 is_released_version = False
