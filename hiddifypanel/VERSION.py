fca3440fe6b77e85429c8ce7865838f1993e5b78
mn-hacker <mahdishokri228@outlook.com>
2026-05-31 17:53:16 +0330

    v12.1.37b: bump version for new singbox binary

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.1.36b'
-__release_date__ = '2026-05-31 16:01'
+__version__ = '12.1.37b'
+__release_date__ = '2026-05-31 17:51'
 __release_time__ = __release_date__
 is_released_version = False
