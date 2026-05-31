0200b867b28e401e5d67619f33a862985951e3a6
mn-hacker <mahdishokri228@outlook.com>
2026-06-01 00:32:38 +0330

    v12.1.44b: bump version for awg double-close fix (h9)

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.1.43b'
-__release_date__ = '2026-06-01 00:09'
+__version__ = '12.1.44b'
+__release_date__ = '2026-06-01 00:32'
 __release_time__ = __release_date__
 is_released_version = False
