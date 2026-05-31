1bfda844c7add919b5352fcf0044cf3b26cfd695
mn-hacker <mahdishokri228@outlook.com>
2026-05-31 23:07:38 +0330

    v12.1.42b: bump version for with_awg singbox binary

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.1.41b'
-__release_date__ = '2026-05-31 22:28'
+__version__ = '12.1.42b'
+__release_date__ = '2026-05-31 23:06'
 __release_time__ = __release_date__
 is_released_version = False
