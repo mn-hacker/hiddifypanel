0f4a8cb3b09df0c5108cfa488014529373b344b3
mn-hacker <mahdishokri228@outlook.com>
2026-06-01 00:10:35 +0330

    v12.1.43b: bump version for awg endpoint fix (h8)

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.1.42b'
-__release_date__ = '2026-05-31 23:06'
+__version__ = '12.1.43b'
+__release_date__ = '2026-06-01 00:09'
 __release_time__ = __release_date__
 is_released_version = False
