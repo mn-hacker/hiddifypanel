d43c53072f65c61f912fccfac5c7b9cb3d270fa9
MN Hacker <mnhacker@example.com>
2026-06-17 00:20:42 +0330

    bump: version 12.1.46b - fix broken form fields (DB migration v136)

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.1.45b'
-__release_date__ = '2026-06-16 18:27'
+__version__ = '12.1.46b'
+__release_date__ = '2026-06-17 00:19'
 __release_time__ = __release_date__
 is_released_version = False
