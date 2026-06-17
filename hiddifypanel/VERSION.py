b7732cd9de2d9286d0d1bba41d5b891957539f35
MN Hacker <mnhacker@example.com>
2026-06-17 13:40:31 +0330

    chore: bump version to 12.1.47b

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.1.46b'
-__release_date__ = '2026-06-17 00:19'
+__version__ = '12.1.47b'
+__release_date__ = '2026-06-17 13:40'
 __release_time__ = __release_date__
 is_released_version = False
