5d7a605e8ec6f467844a44e72f494b54f6435eb8
MN Hacker <mnhacker@example.com>
2026-06-19 20:23:43 +0330

    fix: VERSION.py variables causing import errors; bump to v12.1.51b

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,2 +1,6 @@
-__version__='12.1.50b'
-__updated__='1781897863'
+# This file is auto-generated using common/update_version.py
+# from hiddifypanel import  __version__
+__version__ = '12.1.51b'
+__release_date__ = '2026-06-19 20:25'
+__release_time__ = __release_date__
+is_released_version = False
