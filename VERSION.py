646f649d7bc57df8fbf662836b8911d773d1a493
MN Hacker <mnhacker@example.com>
2026-08-22 00:09:28 +0330

    v12.2.7b - sync versions

--- VERSION.py
+++
@@ -1,6 +0,0 @@
-# This file is auto-generated using common/update_version.py
-# from hiddifypanel import  __version__
-__version__ = '12.2.6b'
-__release_date__ = '2026-08-21'
-__release_time__ = __release_date__
-is_released_version = False
