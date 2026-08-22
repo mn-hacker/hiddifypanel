ea1c5da62ee8b12669eedcc44468f9696270eaf5
MN Hacker <mnhacker@example.com>
2026-08-22 22:54:45 +0330

    v12.2.14b - Final version 2 proxies page

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.2.13b'
+__version__ = '12.2.14b'
 __release_date__ = '2026-08-22'
-__release_time__ = '22:07:36'
+__release_time__ = '22:54:26'
 is_released_version = False
