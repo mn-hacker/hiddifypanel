65d30be93a4e94bd2da852617aeb8a4ac4061a15
MN Hacker <mnhacker@example.com>
2026-08-25 03:09:33 +0330

    v12.2.28b - Final version 2 user page

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.2.27b'
+__version__ = '12.2.28b'
 __release_date__ = '2026-08-25'
-__release_time__ = '01:44:22'
+__release_time__ = '03:09:26'
 is_released_version = False
