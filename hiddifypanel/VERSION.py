4db1c99e992c698acaf59fe7d5ad4e46e3392074
MN Hacker <mnhacker@example.com>
2026-08-25 14:57:30 +0330

    v12.2.29b - Fix bugs

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.2.28b'
+__version__ = '12.2.29b'
 __release_date__ = '2026-08-25'
-__release_time__ = '03:09:26'
+__release_time__ = '14:57:05'
 is_released_version = False
