d4afa8c984dd1c4b4a5d97dcfbbf310e33bb9e0b
MN Hacker <mnhacker@example.com>
2026-08-26 14:32:56 +0330

    v12.2.32b - Fix terminal theme

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # This file is auto-generated using common/update_version.py
 # from hiddifypanel import  __version__
-__version__ = '12.2.31b'
+__version__ = '12.2.32b'
 __release_date__ = '2026-08-26'
-__release_time__ = '13:46:18'
+__release_time__ = '14:32:49'
 is_released_version = False
